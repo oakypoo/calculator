690dcef3fc95929cbc0a9918ac17cdb0c588ef3d
oakypoo <97937395+oakypoo@users.noreply.github.com>
2023-03-28 08:47:16 +0300

    pievienoju funkcijas

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
 
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+console.log (Divide(69,420))
+
+plusins.addEventListener("click", () =>(
+    console.log("mani nospieda")
+))
