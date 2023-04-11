6e0c0edd3a2523f7a9cf45dcfed63a5c514e20ab
oakypoo <97937395+oakypoo@users.noreply.github.com>
2023-04-11 10:06:41 +0300

    Add files via upload

---
+++ Jauna mape (6)/script.js
@@ -0,0 +1,29 @@
+const plusins = document.querySelector('.js-plus');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result');
+
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
+
+
+})
